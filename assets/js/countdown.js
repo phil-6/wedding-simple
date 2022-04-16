231da709b4ceb6e616c9b639040c065483552b1c
phil-6 <github@purpleriver.dev>
2022-04-16 13:18:41 +0100

    update countdown

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,4 +1,4 @@
-const deadline = 'September 09 2023 12:30:00 GMT+0100'
+const endtime = 'September 09 2023 12:30:00 GMT+0100'
 
 function getTimeRemaining(endtime) {
     const total = Date.parse(endtime) - Date.parse(new Date());
@@ -16,23 +16,39 @@ function getTimeRemaining(endtime) {
     };
 }
 
-function initializeClock(id, endtime) {
-    const clock = document.getElementById(id);
-    const daysSpan = clock.querySelector('.days');
-    const hoursSpan = clock.querySelector('.hours');
-    const minutesSpan = clock.querySelector('.minutes');
-    const secondsSpan = clock.querySelector('.seconds');
+function initializeClock(endtime) {
+    const countdown_days = document.querySelector('#countdown_days');
+    const countdown_all = document.querySelector('#countdown_all');
+
+    const daysDaysSpan = countdown_days.querySelector('.days')
+
+    const daysSpan = countdown_all.querySelector('.days');
+    const hoursSpan = countdown_all.querySelector('#hours');
+    const minutesSpan = countdown_all.querySelector('#minutes');
+    const secondsSpan = countdown_all.querySelector('#seconds');
 
     function updateClock() {
         const t = getTimeRemaining(endtime);
 
+        daysDaysSpan.innerHTML = t.days;
         daysSpan.innerHTML = t.days;
-        // hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-        // minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-        // secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
 
         if (t.total <= 0) {
             clearInterval(timeinterval);
+        }
+
+        if (t.days <= 100 || t.hours <= 2) {
+            countdown_days.classList.add('hidden')
+            countdown_all.classList.remove('hidden')
+        }
+
+        if (t.hours > 2 && t.minutes <= 55) {
+            countdown_days.classList.remove('hidden')
+            countdown_all.classList.add('hidden')
         }
     }
 
@@ -40,4 +56,4 @@ function initializeClock(id, endtime) {
     const timeinterval = setInterval(updateClock, 1000);
 }
 
-initializeClock('countdown', deadline);
+initializeClock(endtime);
